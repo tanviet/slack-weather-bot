331f508cedfeee4422141539ec2f6d2aef5ac70b
Viet Nguyen <viethoavang@gmail.com>
2017-04-16 00:27:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Slack Weather Bot
+
+A Slack bot is used to show weather forecast for selected places.
+
+### 1. Install dependencies
+
+```bash
+$ npm install
+```
+
+### 2. Creating Your Private Slash Command
+
+Sign in to your Slack account and choose your command at `<my-team>.slack.com/services/new/slash-commands`. In my case, I entered `/weather` and hit the **Add Slash Command Integration** button to go to the next page. Go ahead and fill out some of the important fields.
+
+- Command: <your-slack-command> (it will be `/weather` in this case).
+- URL: <your-bot-server> (I used ngrok to develop on local environment).
+- Method: <select-method-that-Slack-sends-data-to-your-server>.
+
+For local development purpose, you should install [ngrok](https://ngrok.com). Once installed, run on terminal:
+
+```bash
+$ ngrok http 3000
+```
+
+Get the http link that ngrok generated and fill out it to above URL field.
+
+### 3. Register Google Maps Geocoding key
+
+Follow [this page](https://developers.google.com/maps/documentation/geocoding/get-api-key) to get a API key so that our app can get place's information such as lat, lng...and use them into Forecast API.
+
+### 4. Register Dark Sky key
+
+Sign up a [new account](https://darksky.net/dev/) and get API key to get weather forecast data.
+
+### 5. Setup your credentials on config.js
+
+Create `config.js` file in `server` folder and you should include your credentials into that file.
+
+```javascript
+module.exports = {
+  SLACK_VERIFICATION_TOKEN: '<your-token>',
+  DARK_SKY_API_KEY: '<your-key>',
+  GOOGLE_MAPS_GEOCODING_API_KEY: '<your-key>'
+}
+```
+
+### 6. Run the app
+
+```bash
+$ node server/index.js
+```
+
+And then on Slack's client interface, you can enter `/weather NewYork` to test your command.
---
+++ server/index.js
@@ -0,0 +1,188 @@
+'use strict';
+
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const async = require('async');
+const Forecast = require('forecast');
+const googleMaps = require('@google/maps');
+
+// Init app
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Init weather forecast
+const forecast = new Forecast({
+  service: 'darksky',
+  key: config.DARK_SKY_API_KEY,
+  units: 'celcius',
+  cache: true,      // Cache API requests
+  ttl: {
+    minutes: 27,
+    seconds: 45
+  }
+});
+
+// Init Google Maps to getcode an address
+const googleMapsClient = googleMaps.createClient({
+  key: config.GOOGLE_MAPS_GEOCODING_API_KEY
+});
+
+// Start server
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+
+/* *******************************
+/* Weather Forecast Slash Command
+/* ***************************** */
+
+app.get('/', (req, res) => {
+  handleDataFromSlack(req.query, res);
+});
+
+app.post('/', (req, res) => {
+  handleDataFromSlack(req.body, res);
+});
+
+/**
+ * Outgoing Data
+ *
+ * When a matching chat message is received, a POST will be sent to the URL specified below. The data is defined as follows:
+ *
+ *  token=cIp7eBVRZ*************
+    team_id=***8
+    team_domain=example
+    channel_id=C21****
+    channel_name=test
+    user_id=U21****
+    user_name=Steve
+    command=/weather
+    text=94****
+    response_url=https://hooks.slack.com/commands/1234/5678
+ */
+
+function handleDataFromSlack(data, res) {
+  if (data.token !== config.SLACK_VERIFICATION_TOKEN) {
+    // The request is NOT coming from Slack!
+    return;
+  }
+
+  if (data.text) {
+    var address = data.text.trim();
+
+    async.waterfall([
+      function(callback) {
+        // Get geocoding info (lat, lng) for selected place
+        googleMapsClient.geocode({
+          address: address
+        }, function(err, response) {
+          if (!err) {
+            callback(null, response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng);
+          } else {
+            callback(new Error('Failed getting address info.'));
+          }
+        });
+      },
+      function(lat, lng, callback) {
+        forecast.get([lat, lng], function(err, weather) {
+          if (!err) {
+            callback(null, weather);
+          } else {
+            callback(new Error('Failed getting weather forecast info.'));
+          }
+        });
+      }
+    ], function (err, result) {
+      if (err) {
+        res.status(200).send(err);
+        return;
+      } else {
+
+        // Send weather info to user on Slack client's interface
+        let info = {
+          response_type: 'in_channel', // 'in_channel' => public to the channel, 'ephemeral' => private message,
+          text: result.currently.summary,
+          ts: (new Date()).getTime(),
+          attachments: [
+            {
+              fallback: 'Summary of the weather forecast for ' + address,
+              color: '#36a64f',
+              fields: [
+                {
+                  title: 'Timezone',
+                  value: result.timezone,
+                  short: true
+                },
+                {
+                  title: 'Offset',
+                  value: result.offset,
+                  short: true
+                },
+                {
+                  title: 'The intensity of precipitation',
+                  value: result.currently.precipIntensity + ' (millimeters per hour)',
+                  short: true
+                },
+                {
+                  title: 'The probability of precipitation occurring',
+                  value: (result.currently.precipProbability * 100).toFixed(2) + '%',
+                  short: true
+                },
+                {
+                  title: 'Temperature',
+                  value: result.currently.temperature + ' (Degrees Celsius)',
+                  short: true
+                },
+                {
+                  title: 'The dew point',
+                  value: result.currently.dewPoint + ' (Degrees Celsius)',
+                  short: true
+                },
+                {
+                  title: 'The relative humidity',
+                  value: (result.currently.humidity * 100).toFixed(2) + '%',
+                  short: true
+                },
+                {
+                  title: 'The wind speed',
+                  value: result.currently.windSpeed + ' (miles per hour)',
+                  short: true
+                },
+                {
+                  title: 'The average visibility in miles (capped at 10 miles)',
+                  value: result.currently.visibility + ' (miles)',
+                  short: true
+                },
+                {
+                  title: 'The percentage of sky occluded by clouds',
+                  value: (result.currently.cloudCover * 100).toFixed(2) + '%',
+                  short: true
+                },
+                {
+                  title: 'The sea-level air pressure',
+                  value: result.currently.pressure + ' (millibars)',
+                  short: true
+                },
+                {
+                  title: 'The columnar density of total atmospheric ozone',
+                  value: result.currently.ozone + ' (Dobson units)',
+                  short: true
+                }
+              ]
+            }
+          ]
+        };
+
+        res.json(info);
+      }
+    });
+  } else {
+    res.send('Please enter place you want to view weather forecast.');
+    return;
+  }
+}
+
